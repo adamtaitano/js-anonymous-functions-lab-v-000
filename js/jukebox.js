a7102a1240ad0331fb9a13ccf613b3b536f54739
Adam Taitano <adam.taitano@gmail.com>
2016-09-10 20:04:02 +0000

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-  menu();
-  play();
-  pause();
-  show();
-});
-
 var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
 var artists = Object.keys(jukeboxSongs);
 var theArtist = artists[Math.floor(Math.random()) * artists.length];
@@ -12,33 +5,51 @@ var songToPlay = jukeboxSongs[theArtist];
 
 function randomNumber() {
   return Math.floor(Math.random());
-}
+};
 
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
-}
+  $('#menu').on('click', handleMenuClick);
+};
+
+function handleMenuClick() {
+  $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+};
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
-}
+  $('#play').on('click', handlePlayClick);
+};
+
+function handlePlayClick() {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
+};
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
-}
+  $('#pause').on('click', handlePauseClick);
+};
+
+function handlePauseClick() {
+  $('#songPaused').html(songToPlay + " is paused");
+};
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
-}
+  $('#show').on('click', handleShowClick);
+};
+
+function handleShowClick() {
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+  $('#showSongs').html(songs);
+};
+
+$(document).ready(function(){
+
+  menu();
+  play();
+  pause();
+  show();
+
+
+});
